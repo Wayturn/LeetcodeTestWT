b649936976bf4bae5153f9a7ffed29b41788138d
Bruce <bruce@cmind.com.tw>
2025-12-11 11:24:10 +0800

    新增 42. Trapping Rain Water 解題程式碼

---
+++ 42.TrappingRainWater.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left=0
+        right=len(height)-1
+        left_max=0
+        right_max=0
+        water=0        
+        while left<right:
+            if height[left]<height[right]:
+                left_max = max(height[left],left_max)
+                water+=left_max-height[left]
+                left+=1
+            else:
+                right_max = max(height[right],right_max)
+                water+=right_max-height[right]
+                right-=1
+        return water
